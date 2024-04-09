6ede8a886d51d447ff18a1455f054b59ed878f48
petemurray45 <pmurray697@gmail.com>
2024-04-09 19:09:50 +0100

    final stages of hangman game - just missing lives used

--- main.py
+++ main.py
@@ -5,46 +5,50 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 ran_choice = random.randint(0, 2)
-
 chosen_word = word_list[ran_choice]
-
 print(chosen_word)
-
 display_word = []
 
 for element in chosen_word:
     display_word.append("_")
 
 print(display_word)
-
 guess = input("Guess a letter in the hidden word ").lower()
 
-winner = 0
-
-if display_word == word_list[0]:
-    winner = 1
-elif display_word == word_list[1]:
-    winner = 1
-elif display_word == word_list[2]:
-    winner = 1
-else:
-    you_win = 0
-
-i = 0
-
-while i < len(chosen_word):
-    for letter in chosen_word:
-        if letter == guess:
-            letter_position = chosen_word.index(letter)
-            display_word[letter_position] = letter
-            i += 1
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+    if letter == guess:
+        display_word[position] = letter
 
 print(display_word)
 
+winner = False
 
-for letter in chosen_word:
-    while i < len(chosen_word):
-        if letter == guess:
-            i += 1
-            letter_position = chosen_word.index(letter)
-            display_word[letter_position] = letter
+while not winner:
+    if "_" not in display_word:
+        winner = True
+        break
+    else:
+        second_guess = input("Guess again ")
+        for position in range(len(chosen_word)):
+            letter = chosen_word[position]
+            if letter == second_guess:
+                display_word[position] = letter
+    print(display_word)
+
+if winner:
+    print("You Won!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
