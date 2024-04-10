639e04dad93187acebe68aceacfc9349d33c15b4
petemurray45 <pmurray697@gmail.com>
2024-04-10 12:43:05 +0100

    finished python hangman game

--- main.py
+++ main.py
@@ -1,9 +1,65 @@
 # This is a sample Python script.
-#Step 1
+# Step 1
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
+word_list = ["aardvark", "baboon", "camel"]
 ran_choice = random.randint(0, 2)
 chosen_word = word_list[ran_choice]
 print(chosen_word)
@@ -13,42 +69,25 @@ for element in chosen_word:
     display_word.append("_")
 
 print(display_word)
-guess = input("Guess a letter in the hidden word ").lower()
 
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-        display_word[position] = letter
+end_of_game = False
+winner = 0
+wrong_answer = 0
+player_lives = 6
 
-print(display_word)
+while not end_of_game:
+    guess = input("Guess a letter in the hidden word ").lower()
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display_word[position] = letter
 
-winner = False
+    if guess not in chosen_word:
+        player_lives -= 1
+        print(f"Wrong! You have {player_lives} lives left.")
+        print(stages[player_lives])
+        if player_lives == 0:
+            print("Out of Lives! Try again next time.")
+            end_of_game = True
 
-while not winner:
-    if "_" not in display_word:
-        winner = True
-        break
-    else:
-        second_guess = input("Guess again ")
-        for position in range(len(chosen_word)):
-            letter = chosen_word[position]
-            if letter == second_guess:
-                display_word[position] = letter
     print(display_word)
-
-if winner:
-    print("You Won!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
